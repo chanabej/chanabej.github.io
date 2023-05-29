7c306c61e303f749c1ed68c79e3d035ddd4afe06
Jonathan <jonathanchanabe@hotmail.fr>
2023-05-29 14:25:48 +0200

    advanced

--- script.js
+++ script.js
@@ -56,3 +56,60 @@ const rightArrow = document.querySelector('.slider-navigation__right-arrow');
 leftArrow.addEventListener('click', () => swipe('right'));
 rightArrow.addEventListener('click', () => swipe('left'));
 
+// ...
+
+// Ajouter cette fonction pour créer les pastilles
+function createIndicators(totalSlides) {
+  const indicatorsContainer = document.querySelector('.indicators');
+
+  for (let i = 0; i < totalSlides; i++) {
+    const indicator = document.createElement('div');
+    indicator.classList.add('indicator');
+
+    // Ajouter un écouteur d'événement pour passer à la diapositive correspondante
+    indicator.addEventListener('click', () => {
+      position = i;
+      updateIndicators(totalSlides);
+      slides.style.transform = `translateX(-${position * width}px)`;
+    });
+
+    indicatorsContainer.appendChild(indicator);
+  }
+}
+
+// Ajouter cette fonction pour mettre à jour la pastille active
+function updateIndicators(totalSlides) {
+  const indicators = document.querySelectorAll('.indicator');
+
+  indicators.forEach((indicator, index) => {
+    if (index === position) {
+      indicator.classList.add('active');
+    } else {
+      indicator.classList.remove('active');
+    }
+  });
+}
+
+// Modifier votre fonction swipe pour mettre à jour la pastille active
+function swipe(direction) {
+  const width = slides.querySelector('img').clientWidth;
+  const totalSlides = document.querySelectorAll('.slides img').length;
+
+  if (direction === 'left') {
+    position = (position + 1) % totalSlides;
+  } else if (direction === 'right') {
+    position = (position - 1 + totalSlides) % totalSlides;
+  }
+
+  slides.style.transform = `translateX(-${position * width}px)`;
+  updateIndicators(totalSlides); // Mettre à jour la pastille active
+}
+
+// ...
+
+// Modifier votre code existant pour appeler la fonction createIndicators
+const totalSlides = document.querySelectorAll('.slides img').length;
+createIndicators(totalSlides);
+updateIndicators(totalSlides);
+
+// ...
